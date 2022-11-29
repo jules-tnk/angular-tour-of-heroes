2843084e7f45f710a2fcde17fba2c32083b046ee
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-11-29 11:49:52 +0100

    add navigation bar add navigation to hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 import {HeroService} from "../services/hero/hero.service";
-import {MessageService} from "../services/message/message.service";
 
 
 @Component({
@@ -14,25 +13,16 @@ import {MessageService} from "../services/message/message.service";
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
 
   //CONSTRUCTOR
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
-    if (this.selectedHero==hero){
-      this.selectedHero = undefined;
-      return
-    }
-    this.messageService.add(`HeroesComponent: selected hero name=${hero.name}`);
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
