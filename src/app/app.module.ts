9eddadf65117063b5a9c738b5a9beb54370d4cbf
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2022-12-28 10:02:55 +0100

    Check app module and heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     HeroesComponent,
     HeroDetailComponent,
     MessageComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
